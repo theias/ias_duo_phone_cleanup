2e8c79514885504b5e28a18c6f95bd2fcfaf8bb0
Christopher Peterson <3893680+cspeterson@users.noreply.github.com>
2024-08-08 14:34:41 -0400

    Bump patch version

--- duo_phone_cleanup/__init__.py
+++ duo_phone_cleanup/__init__.py
@@ -1,5 +1,5 @@
 """ duo_phone_cleanup """
 __author__ = "cspeterson,bepstein"
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __title__ = "Duo Phone Cleanup"
 __license__ = "License :: OSI Approved :: MIT License"
